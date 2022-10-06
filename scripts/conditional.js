f75465d82839ff3fa216ba064ee025ca45274b45
shruthi093 <jayasruthi.l@gmail.com>
2022-10-06 21:28:22 +0530

    v1.0

--- scripts/conditional.js
+++ scripts/conditional.js
@@ -42,5 +42,68 @@ else if(wish==='juice'){
 }
 else{
    console.log('give some water')
+}
+let age=18
+switch(age){
+    case 10:
+        console.log("age is 10")
+    case 18:
+        console.log("age is 18")
+        console.log('you are eligible')
+
+}
+let day=8
+switch(day){
+    case 0:
+        console.log('sunday')
+        break
+    case 1:
+        console.log('monday')
+        break
+    case 2:
+        console.log('tuesday')
+        break
+    default:
+        console.log('day order does not exixts')
+        break  
+    
+}
+let department="CSE"
+switch(department){
+    case "CSE":
+        console.log("go to second floor")
+        break
+    case "MECH":
+        console.log("go to third floor")
+        break
+    case "ECE":
+        console.log("go to first floor")
+        break
+    default:
+        console.log("wrong input") 
+}
+let number=10
+switch(number>0)
+{
+    case true:
+        console.log("positive no")
+        break
+    case false:
+        console.log("negative no")
+        break
 }*/
+
+//scope
+// let vs var
+let country="India"//global 
+if(country==="India")
+{
+   // var state="Kerala" -> it will be accessed outside the block -> not right , use let
+let state="Kerala"//local to the block
+console.log(`the country is ${country} and the state is ${state}`)
+}
+console.log(`the country is ${country} and the state is ${state}`)
+
+
+
 
